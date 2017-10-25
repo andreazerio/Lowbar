ece23363fe2820ed32394ed7d3f50e9a463d196a
Andrea Zerio <andreazerio@andreas-mbp.home>
2017-10-25 21:49:37 +0100

    implementing first method and testing

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ const _ = {};
 
 _.identity = (val) => val;
 
+_.first = (arr, num = 1) => {
+if (typeof arr === 'string') arr = arr.split('');
+if (!Array.isArray(arr) || arr.length === 0) return undefined;
+return num === 1 ? arr[0] : arr.slice(0,num);
+};
+
 module.exports = _;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -10,7 +10,7 @@ describe('_', () => {
     });
 });
 
-describe('#identity', () => {
+describe('identity', () => {
     it('exists', () => {
         expect(_.identity).to.be.a('function');
     });
@@ -18,5 +18,24 @@ describe('#identity', () => {
         expect(_.identity([1,2,3,4,5])).to.eql([1,2,3,4,5]);
         expect(_.identity(3)).to.equal(3);
         expect(_.identity()).to.equal(undefined);
+    });
+});
+
+describe('first', () => {
+    it('exists', () => {
+        expect(_.first).to.be.a('function');
+    }),
+    it('returns the first n elements of the array', () => {
+        expect(_.first([1,2,3])).to.equal(1);
+        expect(_.first([1,2,3],2)).to.eql([1,2]);
+    });
+    it('returns the first n elements of a string', () => {
+        expect(_.first('hello'), 1).to.eql('h');
+    });
+    it('returns undefined if passed the wrong arguments', () => {
+        expect(_.first()).to.eql(undefined);
+        expect(_.first(12)).to.eql(undefined);
+        expect(_.first(true)).to.eql(undefined);
+        expect(_.first([])).to.eql(undefined);
     });
 });
